b33adc72f69463477658e65a4200ac3de892f0c8
pinkman7009 <soumik.chaudhuri2000@gmail.com>
2021-03-05 00:50:14 +0530

    fixed play button bug

--- timer.js
+++ timer.js
@@ -1,48 +1,66 @@
 class Timer {
-	constructor (durationInput, startButton, pauseButton, callbacks) {
-		this.durationInput = durationInput;
-		this.startButton = startButton;
-		this.pauseButton = pauseButton;
+  constructor(
+    durationInput,
+    startButton,
+    pauseButton,
+    alreadyClicked,
+    callbacks
+  ) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+    this.alreadyClicked = alreadyClicked;
 
-		if (callbacks) {
-			this.onStart = callbacks.onStart;
-			this.onTick = callbacks.onTick;
-			this.onComplete = callbacks.onComplete;
-		}
-		this.startButton.addEventListener('click', this.start);
-		this.pauseButton.addEventListener('click', this.pause);
-	}
+    if (callbacks) {
+      this.onStart = callbacks.onStart;
+      this.onTick = callbacks.onTick;
+      this.onComplete = callbacks.onComplete;
+    }
 
-	start = () => {
-		if (this.onStart) {
-			this.onStart(this.timeRemaining);
-		}
+    this.startButton.addEventListener('click', this.start);
 
-		this.intervalID = setInterval(() => {
-			this.tick();
-		}, 20);
-	};
+    this.pauseButton.addEventListener('click', this.pause);
+  }
 
-	pause = () => {
-		clearInterval(this.intervalID);
-	};
+  start = () => {
+    if (this.alreadyClicked === false) {
+      if (this.onStart) {
+        this.onStart(this.timeRemaining);
+      }
+    }
+    if (this.alreadyClicked === false) {
+      this.alreadyClicked = true;
 
-	tick = () => {
-		if (this.timeRemaining <= 0) {
-			if (this.onComplete) this.onComplete();
-			this.pause();
-		} else {
-			this.timeRemaining = this.timeRemaining - 0.02;
+      this.intervalID = setInterval(() => {
+        this.tick();
+      }, 20);
+    }
+  };
 
-			if (this.onTick) this.onTick(this.timeRemaining);
-		}
-	};
+  pause = () => {
+    this.alreadyClicked = false;
+    clearInterval(this.intervalID);
+  };
 
-	get timeRemaining () {
-		return parseFloat(this.durationInput.value);
-	}
+  tick = () => {
+    if (this.timeRemaining <= 0) {
+      if (this.onComplete) {
+        this.alreadyClicked = false;
+        this.onComplete();
+      }
+      this.pause();
+    } else {
+      this.timeRemaining = this.timeRemaining - 0.02;
 
-	set timeRemaining (time) {
-		durationInput.value = time.toFixed(2);
-	}
+      if (this.onTick) this.onTick(this.timeRemaining);
+    }
+  };
+
+  get timeRemaining() {
+    return parseFloat(this.durationInput.value);
+  }
+
+  set timeRemaining(time) {
+    durationInput.value = time.toFixed(2);
+  }
 }
